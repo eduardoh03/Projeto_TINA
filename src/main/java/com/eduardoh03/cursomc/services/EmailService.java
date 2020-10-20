b5f61970f3fbadfc416724d4e0019e064575c811
Eduardo Henrique <eduhenriquesrs@gmail.com>
2020-10-20 09:59:59 -0300

    Email HTML

--- src/main/java/com/eduardoh03/cursomc/services/EmailService.java
+++ src/main/java/com/eduardoh03/cursomc/services/EmailService.java
@@ -1,13 +1,17 @@
 package com.eduardoh03.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.eduardoh03.cursomc.domain.Pedido;
 
 public interface EmailService {
 
-	void sendOrderConfirmationEmail(Pedido obj);
-	
+	void sendOrderConfirmationEmail(Pedido obj);	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	void sendHtmlEmail(MimeMessage msg);
 	
 }
