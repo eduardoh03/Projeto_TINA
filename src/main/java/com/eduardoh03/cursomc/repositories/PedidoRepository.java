3e8fd3e1407735f78ea83bd2a0816e9f5fc664c5
Eduardo Henrique <eduhenriquesrs@gmail.com>
2020-10-22 11:28:11 -0300

    Restricao de conteudo: cliente so recupera seus pedidos

--- src/main/java/com/eduardoh03/cursomc/repositories/PedidoRepository.java
+++ src/main/java/com/eduardoh03/cursomc/repositories/PedidoRepository.java
@@ -1,9 +1,16 @@
 package com.eduardoh03.cursomc.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.eduardoh03.cursomc.domain.Cliente;
 import com.eduardoh03.cursomc.domain.Pedido;
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer>{
+	
+	@Transactional(readOnly=true)
+	Page<Pedido> findByCliente(Cliente cliente, Pageable pageRequest);
 	}
--- src/main/java/com/eduardoh03/cursomc/services/PedidoService.java
+++ src/main/java/com/eduardoh03/cursomc/services/PedidoService.java
@@ -6,8 +6,12 @@ import java.util.Optional;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.eduardoh03.cursomc.domain.Cliente;
 import com.eduardoh03.cursomc.domain.ItemPedido;
 import com.eduardoh03.cursomc.domain.PagamentoComBoleto;
 import com.eduardoh03.cursomc.domain.Pedido;
@@ -15,37 +19,40 @@ import com.eduardoh03.cursomc.domain.enums.EstadoPagamento;
 import com.eduardoh03.cursomc.repositories.ItemPedidoRepository;
 import com.eduardoh03.cursomc.repositories.PagamentoRepository;
 import com.eduardoh03.cursomc.repositories.PedidoRepository;
+import com.eduardoh03.cursomc.security.UserSS;
+import com.eduardoh03.cursomc.services.exceptions.AuthorizationException;
 import com.eduardoh03.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class PedidoService {
-	
+
 	@Autowired
 	private PedidoRepository repo;
-	
+
 	@Autowired
 	private BoletoService boletoService;
-	
+
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
-	
+
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
-	
+
 	@Autowired
 	private ProdutoService produtoService;
-	
+
 	@Autowired
 	private ClienteService clienteService;
-	
+
 	@Autowired
 	private EmailService emailService;
-	
+
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
 	}
+
 	@Transactional
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
@@ -69,4 +76,14 @@ public class PedidoService {
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
 	}
+
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
+}
